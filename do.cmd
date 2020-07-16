c37d42a81d5c7deae7f589b33168518ce1ee5ba6
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2020-07-16 23:08:18 +0200

    optimize SVG files

--- do.cmd
+++ do.cmd
@@ -3,6 +3,7 @@
 if [%1] == [help] (
   echo  w $V:   set gradle wrapper
   echo  fixgit: fix permission flag on git index for required files
+  echo  svgo:   optimize SVG icons with SGVO. SVGO must be present, type 'npm install -g svgo' if needed)
 )
 
 if [%1] == [w] (
@@ -11,4 +12,7 @@ if [%1] == [w] (
 if [%1] == [fixgit] (
   echo git update-index --chmod=+x gradlew
   git update-index --chmod=+x gradlew
+)
+if [%1] == [svgo] (
+  svgo --folder=resources/icons/ --multipass --config=svgo.yml
 )
