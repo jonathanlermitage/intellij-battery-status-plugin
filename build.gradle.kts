ffd2937e2ee4a00cca0c0f79adf277d105adff67
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2021-03-07 00:38:52 +0100

    migrate gradle config to kt dsl, cleanup

---
+++ build.gradle.kts
@@ -0,0 +1,87 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
+plugins {
+    id("java")
+    id("idea")
+    id("org.jetbrains.intellij") version "0.7.2" // https://github.com/JetBrains/gradle-intellij-plugin
+    id("com.github.ben-manes.versions") version "0.38.0" // https://github.com/ben-manes/gradle-versions-plugin
+}
+
+// Import variables from gradle.properties file
+val pluginIdeaVersion: String by project
+val pluginDownloadIdeaSources: String by project
+val pluginInstrumentPluginCode: String by project
+val pluginVersion: String by project
+val pluginJavaVersion: String by project
+val pluginEnableBuildSearchableOptions: String by project
+
+val inCI = System.getenv("CI") != null
+
+println("Will use IDEA $pluginIdeaVersion and Java $pluginJavaVersion")
+
+group = "lermitage.intellij.battery.status"
+version = pluginVersion
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
+}
+
+intellij {
+    downloadSources = pluginDownloadIdeaSources.toBoolean() && !inCI
+    instrumentCode = pluginInstrumentPluginCode.toBoolean()
+    pluginName = "Battery Status"
+    sandboxDirectory = "${rootProject.projectDir}/.idea-sandbox/${pluginIdeaVersion}"
+    updateSinceUntilBuild = false
+    version = pluginIdeaVersion
+}
+
+tasks {
+    withType<JavaCompile> {
+        sourceCompatibility = pluginJavaVersion
+        targetCompatibility = pluginJavaVersion
+        options.compilerArgs = listOf("-Xlint:deprecation")
+    }
+    withType<Test> {
+        useJUnitPlatform()
+    }
+    withType<DependencyUpdatesTask> {
+        checkForGradleUpdate = true
+        gradleReleaseChannel = "current"
+        outputFormatter = "plain"
+        outputDir = "build"
+        reportfileName = "dependencyUpdatesReport"
+        revision = "release"
+        resolutionStrategy {
+            componentSelection {
+                all {
+                    if (isNonStable(candidate.version)) {
+                        println(" - [ ] ${candidate.module}:${candidate.version} candidate rejected")
+                        reject("Not stable")
+                    } else {
+                        println(" - [X] ${candidate.module}:${candidate.version} candidate accepted")
+                    }
+                }
+            }
+        }
+    }
+    runIde {
+        jvmArgs = listOf("-Xms768m", "-Xmx2048m", "--add-exports", "java.base/jdk.internal.vm=ALL-UNNAMED")
+    }
+    buildSearchableOptions {
+        enabled = pluginEnableBuildSearchableOptions.toBoolean()
+        jvmArgs = listOf("--add-exports", "java.base/jdk.internal.vm=ALL-UNNAMED")
+    }
+}
+
+fun isNonStable(version: String): Boolean {
+    if (listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().endsWith(it) }) {
+        return false
+    }
+    return listOf("alpha", "Alpha", "ALPHA", "b", "beta", "Beta", "BETA", "rc", "RC", "M", "EA", "pr", "atlassian").any {
+        "(?i).*[.-]${it}[.\\d-]*$".toRegex().matches(version)
+    }
+}
--- do.cmd
+++ do.cmd
@@ -1,19 +1,41 @@
 @echo off
 
 if [%1] == [help] (
-  echo  w $V:   set gradle wrapper
-  echo  fixgit: fix permission flag on git index for required files
-  echo  svgo:   optimize SVG icons with SGVO. SVGO must be present, type 'npm install -g svgo' if needed)
+  echo w $V:    set gradle wrapper
+  echo fixgit:  fix permission flag on git index for required files
+  echo run :    run plugin in IntelliJ Ultimate
+  echo runeap:  run plugin in latest IntelliJ EAP Snapshot
+  echo release: package plugin
+  echo test:    run unit tests
+  echo cv:      check dependencies and Gradle updates
+  echo svgo:    optimize SVG icons with SGVO. SVGO must be present, type 'npm install -g svgo' if needed
 )
 
 if [%1] == [w] (
   gradle wrapper --gradle-version=%2 --no-daemon
 )
 if [%1] == [fixgit] (
-  echo git update-index --chmod=+x gradlew
   git update-index --chmod=+x gradlew
+  echo "gradlew" has now executable flag on git
+  git update-index --chmod=+x do
+  echo "do" has now executable flag on git
+)
+if [%1] == [run] (
+  gradlew buildPlugin runIde --warning-mode all
+)
+if [%1] == [runeap] (
+  gradlew buildPlugin runIde --warning-mode all -PpluginIdeaVersion=IC-LATEST-EAP-SNAPSHOT -PpluginDownloadIdeaSources=false
+)
+if [%1] == [release] (
+  gradlew clean buildPlugin test verifyPlugin --warning-mode all
+)
+if [%1] == [test] (
+  gradlew cleanTest test verifyPlugin --warning-mode all
+)
+if [%1] == [cv] (
+  gradlew dependencyUpdates --warning-mode all
 )
 if [%1] == [svgo] (
-  svgo --folder=resources/icons/set1/ --multipass --config=svgo.yml
-  svgo --folder=resources/icons/set2/ --multipass --config=svgo.yml
+  svgo --folder=src/main/resources/icons/set1/ --multipass --config=svgo.yml
+  svgo --folder=src/main/resources/icons/set2/ --multipass --config=svgo.yml
 )
