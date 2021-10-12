827d32498215a0746303ded51b1b4fb179440e6d
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2021-10-13 00:29:55 +0200

    v1.21 Fix battery status not refreshing when the day is changing

--- src/main/java/lermitage/intellij/battery/status/core/BatteryReader.java
+++ src/main/java/lermitage/intellij/battery/status/core/BatteryReader.java
@@ -4,13 +4,13 @@ package lermitage.intellij.battery.status.core;
 
 import lermitage.intellij.battery.status.cfg.SettingsService;
 
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 
 import static lermitage.intellij.battery.status.cfg.SettingsService.MINIMAL_REFRESH_INTERVAL;
 
 public class BatteryReader {
 
-    private static LocalTime lastCall;
+    private static LocalDateTime lastCall;
     private static String lastBatteryStatus;
     private final static OS detectedOS = OS.detectOS();
     private static final int CACHE_DURATION_SEC = MINIMAL_REFRESH_INTERVAL / 1000;
@@ -21,7 +21,7 @@ public class BatteryReader {
      * @return battery status.
      */
     public static synchronized String getBatteryStatus(SettingsService settingsService) {
-        if (lastBatteryStatus != null && lastCall != null && lastCall.plusSeconds(CACHE_DURATION_SEC).isAfter(LocalTime.now())) {
+        if (lastBatteryStatus != null && lastCall != null && lastCall.plusSeconds(CACHE_DURATION_SEC).isAfter(LocalDateTime.now())) {
             return lastBatteryStatus;
         }
         switch (detectedOS) {
@@ -35,7 +35,7 @@ public class BatteryReader {
             default:
                 lastBatteryStatus = BatteryUtils.readLinuxBatteryStatus(settingsService.getLinuxBatteryCommand());
         }
-        lastCall = LocalTime.now();
+        lastCall = LocalDateTime.now();
         return lastBatteryStatus;
     }
 }
